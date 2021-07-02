b4a94a60df0bc8bc72c9a715195d6490dcdaf522
Tlaster <tlaster@outlook.com>
2021-07-02 11:56:02 +0800

    update compose rc01

--- placeholder/build.gradle.kts
+++ placeholder/build.gradle.kts
@@ -41,7 +41,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "moe.tlaster"
                 artifactId = "placeholder"
-                version = "0.6.0"
+                version = "0.7.0"
 
                 from(components["release"])
             }
